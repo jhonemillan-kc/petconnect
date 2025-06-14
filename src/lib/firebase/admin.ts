96bb77c24d2b743f19a39c769678fd41d47b4277
Jhon Millan <jhonmillan@MacBook-Pro-de-Jhon.local>
2025-06-13 23:36:17 -0500

    fix name

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 import type { Firestore } from 'firebase-admin/firestore';
 import * as fs from 'fs';
-const serviceAccount = JSON.parse(fs.readFileSync('./firebase-keys.json', 'utf8'));
+const serviceAccount = JSON.parse(fs.readFileSync('./firebase-info.json', 'utf8'));
 
 let adminDB: Firestore | undefined;
 
